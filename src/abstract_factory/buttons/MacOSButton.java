999c99fa2064c5dd2c907a9eb73e23ea8d948088
Ngo Minh Tri <ngominhtrint@gmail.com>
2023-07-11 10:31:24 -0500

    Implement abstract factory and builder design patterns

---
+++ src/abstract_factory/buttons/MacOSButton.java
@@ -0,0 +1,9 @@
+package abstract_factory.buttons;
+
+public class MacOSButton implements Button {
+
+    @Override
+    public void paint() {
+        System.out.println("You have created MacOSButton.");
+    }
+}
---
+++ src/builder/Demo.java
@@ -0,0 +1,24 @@
+package builder;
+
+import builder.builders.CarBuilder;
+import builder.builders.CarManualBuilder;
+import builder.cars.Car;
+import builder.cars.Manual;
+import builder.director.Director;
+
+public class Demo {
+    
+    public static void main(String[] args) throws Exception {
+        Director director = new Director();
+
+        CarBuilder builder = new CarBuilder();
+        director.constructSportCar(builder);
+        Car car = builder.getResult();
+        System.out.println("Car built:\n" + car.getCarType());
+
+        CarManualBuilder manualBuilder = new CarManualBuilder();
+        director.constructSportCar(manualBuilder);
+        Manual carManual = manualBuilder.getResult();
+        System.out.println("\nCar manual built:\n" + carManual.print());
+    }
+}
