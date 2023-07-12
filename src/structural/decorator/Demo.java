fd176ae2060a445d3a5822a790b264f761963dfa
Ngo Minh Tri <ngominhtrint@gmail.com>
2023-07-11 21:46:50 -0500

    add Decorator design pattern

---
+++ src/structural/decorator/Demo.java
@@ -0,0 +1,26 @@
+package structural.decorator;
+
+import structural.decorator.decorators.CompressionDecorator;
+import structural.decorator.decorators.DataSource;
+import structural.decorator.decorators.DataSourceDecorator;
+import structural.decorator.decorators.EncryptionDecorator;
+import structural.decorator.decorators.FileDataSource;
+
+public class Demo {
+    public static void main(String[] args) {
+        String filePath = "OutputDemo.txt";
+        String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
+        DataSourceDecorator encoded = new CompressionDecorator(
+                                         new EncryptionDecorator(
+                                             new FileDataSource(filePath)));
+        encoded.writeData(salaryRecords);
+        DataSource plain = new FileDataSource(filePath);
+
+        System.out.println("- Input ----------------");
+        System.out.println(salaryRecords);
+        System.out.println("- Encoded --------------");
+        System.out.println(plain.readData());
+        System.out.println("- Decoded --------------");
+        System.out.println(encoded.readData());
+    }
+}
